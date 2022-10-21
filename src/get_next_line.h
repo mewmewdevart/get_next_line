7b6840d82dcbbc05cae8124a583349e9ef2ff474
Larcrist <larcrist@student.42sp.org.nr>
2022-10-21 17:51:18 -0300

    small tests

--- src/get_next_line.h
+++ src/get_next_line.h
@@ -6,15 +6,16 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 23:00:25 by marvin            #+#    #+#             */
-/*   Updated: 2022/10/10 23:00:25 by marvin           ###   ########.fr       */
+/*   Updated: 2022/10/21 17:10:03 by larcrist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <unistd.h> //write and read
+# include <unistd.h> //write
 # include <stdlib.h> //malloc(), free() and size_t
+# include <fcntl.h> //manipulate file descriptor
 
 /* -------◊	GNL	◊------- */
 char	*get_next_line(int fd);
