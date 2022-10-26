8c42fe27b5ab00ecc662b7779ef9edfe7f42dac8
larcrist@Home <larcrist42@gmail.com>
2022-10-25 22:22:05 -0300

    sobe

--- src/get_next_line.c
+++ src/get_next_line.c
@@ -6,8 +6,17 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 23:00:23 by marvin            #+#    #+#             */
-/*   Updated: 2022/10/10 23:00:23 by marvin           ###   ########.fr       */
+/*   Updated: 2022/10/25 22:21:54 by larcrist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+//statoic function to clean 
+
+char	*get_next_line(int fd)
+{
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (NULL);
+
+}
