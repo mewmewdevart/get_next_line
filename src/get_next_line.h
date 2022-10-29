d200739bcc7cb5dffc531bcef99949685b3fba7e
larcrist@Home <larcrist42@gmail.com>
2022-10-28 23:07:00 -0300

    header more clean

--- src/get_next_line.h
+++ src/get_next_line.h
@@ -17,16 +17,12 @@
 # include <stdlib.h> //malloc(), free() and size_t
 
 /* -------◊	GNL	◊------- */
-int				ft_found_error(int fd);
-void			ft_free(char **str);
-char			*ft_slice(char **str);
-char			*ft_substr(char const *str, unsigned int start, size_t len);
-char			*get_next_line(int fd);
+char				*get_next_line(int fd);
 /* -------◊	UTILS	◊------- */
-size_t			ft_strlen(const char *s);
-char			*ft_strchr(const char *s, int c);
-char			*ft_strjoin(char const *s1, char const *s2);
-size_t			ft_strlcpy(char *dst, const char *src, size_t size);
-char			*ft_strdup(const char *s1);
+size_t				ft_strlen(const char *s);
+char				*ft_strchr(const char *s, int c);
+char				*ft_strjoin(char const *s1, char const *s2);
+size_t				ft_strlcpy(char *dst, const char *src, size_t size);
+char				*ft_strdup(const char *s1);
 
 #endif
