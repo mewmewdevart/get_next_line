d34b859d4691f7e966f6d88785b6d0d8c27de30a
larcrist@Home <larcrist42@gmail.com>
2022-10-29 11:21:50 -0300

    90% finished

--- src/get_next_line.c
+++ src/get_next_line.c
@@ -6,17 +6,112 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 23:00:23 by marvin            #+#    #+#             */
-/*   Updated: 2022/10/25 22:21:54 by larcrist         ###   ########.fr       */
+/*   Updated: 2022/10/28 21:34:04 by larcrist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-//statoic function to clean 
+static int			ft_found_error(int fd);
+static void			ft_free(char **str);
+static char			*ft_slice(char **str);
+static char			*ft_substr(char const *str, unsigned int start, size_t len);
 
 char	*get_next_line(int fd)
 {
-	if (fd < 0 || BUFFER_SIZE < 1)
-		return (NULL);
+	int			size;
+	char		*buffer;
+	char		*support;
+	static char	*string;
 
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	size = read(fd, buffer, BUFFER_SIZE);
+	if (ft_found_error(fd))
+		return (NULL);
+	while (size > 0)
+	{
+		buffer[size] = '\0';
+		if (!string)
+			string = ft_strdup(buffer);
+		else
+		{
+			support = ft_strjoin(string, buffer);
+			ft_free(&string);
+			string = support;
+		}
+		if (ft_strchr(string, '\n'))
+			break ;
+		size = read(fd, buffer, BUFFER_SIZE);
+	}
+	return (ft_free(&buffer), ft_slice(&string));
+}
+
+//finds an error in input
+static int	ft_found_error(int fd)
+{
+	if (fd == -1 || BUFFER_SIZE < 1)
+		return (1);
+	return (0);
+}
+
+//de-allocate the memory
+static	void	ft_free(char **str)
+{
+	if (str[0])
+	{
+		free(str[0]);
+		str[0] = NULL;
+	}
+}
+
+//slice the string into pieces
+static char	*ft_slice(char **str)
+{
+	int		i;
+	char	*ret;
+	char	*aux;
+
+	i = 0;
+	if (!str[0])
+		return (NULL);
+	while (str[0][i] != '\n' && str[0][i] != '\0')
+		i++;
+	ret = ft_substr(str[0], 0, i + 1);
+	aux = ft_strdup(str[0]);
+	ft_free(str);
+	str[0] = ft_substr(aux, i + 1, ft_strlen(aux));
+	ft_free(&aux);
+	if (!ft_strchr(ret, '\n'))
+	{
+		if (!ft_strlen(ret))
+			ft_free(&ret);
+		ft_free(str);
+	}
+	return (ret);
+}
+
+//returns a substring from a string
+static char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+
+	if (!s)
+		return (NULL);
+	if (ft_strlen(s) < start)
+	{
+		str = (char *)malloc(sizeof(*s));
+		str[0] = '\0';
+		return (str);
+	}
+	if (ft_strlen(s) < start)
+		len = 0;
+	if (ft_strlen(s + start) < len)
+		len = ft_strlen(s + start);
+	str = (char *)malloc(len + 1);
+	if (!str)
+	{
+		return (NULL);
+	}
+	ft_strlcpy(str, s + start, len + 1);
+	return (str);
 }
--- src/get_next_line_utils.c
+++ src/get_next_line_utils.c
@@ -6,8 +6,93 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 23:00:20 by marvin            #+#    #+#             */
-/*   Updated: 2022/10/10 23:00:20 by marvin           ###   ########.fr       */
+/*   Updated: 2022/10/28 21:32:42 by larcrist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+//calculate the length of a string
+size_t	ft_strlen(const char *s)
+{
+	size_t	index;
+
+	index = 0;
+	while (s[index] != '\0')
+		index++;
+	return (index);
+}
+
+//locate character in string
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s != '\0' && *s != (unsigned char)c)
+		s++;
+	if (*s == (unsigned char) c)
+		return ((char *)s);
+	return (NULL);
+}
+
+//concatenates two strings
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	len_s1;
+	size_t	len_s2;
+
+	str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!str)
+		return (0);
+	len_s1 = 0;
+	while (s1[len_s1] != '\0')
+	{
+		str[len_s1] = s1[len_s1];
+		len_s1++;
+	}
+	len_s2 = 0;
+	while (s2[len_s2] != '\0')
+	{
+		str[len_s1 + len_s2] = s2[len_s2];
+		len_s2++;
+	}
+	str[len_s1 + len_s2] = '\0';
+	return (str);
+}
+
+//concatenate string to an specific size
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	index;
+	size_t	len_src;
+
+	len_src = ft_strlen(src);
+	if (size == 0)
+		return (len_src);
+	index = 0;
+	while (index < (size - 1) && src[index] != '\0')
+	{
+		dst[index] = src[index];
+		index++;
+	}
+	dst[index] = '\0';
+	return (len_src);
+}
+
+//creates a dupplicate for the string passed as parameter
+char	*ft_strdup(const char *s1)
+{
+	int		index;
+	int		size;
+	char	*s2;
+
+	size = ft_strlen(s1);
+	s2 = malloc (sizeof(char ) * (size + 1));
+	index = 0;
+	while (s1[index] != '\0')
+	{
+		s2[index] = s1[index];
+		index++;
+	}
+	s2[index] = '\0';
+	return (s2);
+}
