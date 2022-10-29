97c6b58b7b8b0a7ff90ca1c5a0c017189be69b2b
larcrist@Home <larcrist42@gmail.com>
2022-10-29 11:23:27 -0300

    main udates

--- src/main.c
+++ src/main.c
@@ -6,42 +6,38 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 23:00:28 by marvin            #+#    #+#             */
-/*   Updated: 2022/10/26 19:24:44 by larcrist         ###   ########.fr       */
+/*   Updated: 2022/10/29 11:23:15 by larcrist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h> 
-#include <unistd.h>
-#include <fcntl.h> 
 #include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    int fd;
-    char    *line;
+	int		fd;
+	char	*line;
 
-    (void)argc;
-    fd = open(argv[1], O_RDONLY);
-    if (fd == -1)
-    {
-        printf("open() error\n");
-        return (1);
-    }
-    
-    line = "";
-    while (line != NULL)
-    {
+	(void)argc;
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		printf("open() error\n");
+		return (1);
+	}
+	line = "";
+	while (line != NULL)
+	{
 		line = get_next_line(fd);
-		printf("%s\n", line);
-    }
-
-    fd = close(fd);
-    if (fd == -1)
-    {
-        printf("close() error\n");
-        return(1);
-    }
-    printf("%d", fd);
-	
-    return(0);
+		printf("%s", line);
+	}
+	fd = close(fd);
+	if (fd == -1)
+	{
+		printf("close() error\n");
+		return (1);
+	}
+	printf("%d", fd);
+	return (0);
 }
