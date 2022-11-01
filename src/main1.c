3270c8d3026b6e15f226a96cd7d1ff70e76aa2c7
larcrist@Home <larcrist42@gmail.com>
2022-11-01 20:04:39 -0300

    update in main1

--- src/main1.c
+++ src/main1.c
@@ -1,22 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main1.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: larcrist <larcrist@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/01 20:00:38 by larcrist          #+#    #+#             */
+/*   Updated: 2022/11/01 20:04:28 by larcrist         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h> 
 #include <fcntl.h> 
 #include <unistd.h> 
 #include "get_next_line.h" 
 
-int    main(void)
+int	main(void)
 {
-    int    fd;
+	int	fd;
 
-    fd = open("../tests/archive_music.txt", O_RDWR);
-    
-    if (fd == -1)
-        return (-1);
-
-    printf("%s", get_next_line(fd));
-    printf("%s", get_next_line(fd)); //chama-se varias veze
-    printf("%s", get_next_line(fd));
-    close(fd);
-
-    printf("%i", fd);
-    return (0);
+	fd = open("../tests/archive_music.txt", O_RDWR);
+	if (fd == -1)
+		return (-1);
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	close(fd);
+	printf("%i", fd);
+	return (0);
 }
