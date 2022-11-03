0fa06be7a8db0bac92056dcd1b938504f85be312
larcrist@Home <larcrist42@gmail.com>
2022-11-03 11:00:09 -0300

    main bonus

--- src/main_bonus.c
+++ src/main_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main1.c                                            :+:      :+:    :+:   */
+/*   main_bonus.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: larcrist <larcrist@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/01 20:00:38 by larcrist          #+#    #+#             */
-/*   Updated: 2022/11/01 20:04:28 by larcrist         ###   ########.fr       */
+/*   Created: 2022/11/03 10:51:45 by larcrist          #+#    #+#             */
+/*   Updated: 2022/11/03 10:59:14 by larcrist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,26 @@
 
 int	main(void)
 {
-	int	fd;
+	int	fd1;
+	int	fd2;
+	int	fd3;
+	int	fd4;
 
-	fd = open("../tests/archive_music.txt", O_RDWR);
-	if (fd == -1)
+	fd1 = open("../tests/archive_unholy.txt", O_RDWR);
+	fd2 = open("../tests/archive_unholy.txt", O_RDWR);
+	fd3 = open("../tests/archive_unholy.txt", O_RDWR);
+	fd4 = open("../tests/archive_unholy.txt", O_RDWR);
+	if (fd1 == -1 || fd2 == -1 || fd3 == -1 || fd4 == -1)
 		return (-1);
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	close(fd);
-	printf("%i", fd);
+
+	printf("[%d] %s", fd1, get_next_line(fd1));
+	printf("[%d] %s", fd2, get_next_line(fd2));
+	printf("[%d] %s", fd3, get_next_line(fd3));
+	printf("[%d] %s", fd4, get_next_line(fd4));
+
+	close(fd1);
+	close(fd2);
+	close(fd3);
+	close(fd4);
 	return (0);
 }
