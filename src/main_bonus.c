2a4d19ee5ba8dda299cfec8c2974c257cc2c726c
larcrist@Home <larcrist42@gmail.com>
2022-11-04 22:26:59 -0300

    whepa

--- src/main_bonus.c
+++ src/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: larcrist <larcrist@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 10:51:45 by larcrist          #+#    #+#             */
-/*   Updated: 2022/11/03 10:59:14 by larcrist         ###   ########.fr       */
+/*   Updated: 2022/11/04 22:25:42 by larcrist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,12 +28,10 @@ int	main(void)
 	fd4 = open("../tests/archive_unholy.txt", O_RDWR);
 	if (fd1 == -1 || fd2 == -1 || fd3 == -1 || fd4 == -1)
 		return (-1);
-
 	printf("[%d] %s", fd1, get_next_line(fd1));
 	printf("[%d] %s", fd2, get_next_line(fd2));
 	printf("[%d] %s", fd3, get_next_line(fd3));
 	printf("[%d] %s", fd4, get_next_line(fd4));
-
 	close(fd1);
 	close(fd2);
 	close(fd3);
